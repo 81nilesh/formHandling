53d6d69cca0f63d54c42faddc03c8ec35c19ccc7
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-06-01 15:49:55 +0530

    error checking

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm();
 
   const delay = (d) => {
@@ -25,14 +25,16 @@ function App() {
 
   return (
     <>
+      {isSubmitting && <div>Loading....</div>}
       <div className="container">
         <form action="" onSubmit={handleSubmit(onSubmit)}>
           <input placeholder='username' {...register("username", { required: { value: true, message: "This field is required" }, minLength: { value: 3, message: "Min length is 3" }, maxLength: { value: 16, message: "Max length is 16" } })} type="text" />
           {errors.username && <div className='text'>{errors.username.message}</div>}
           <br />
-          <input placeholder='password' {...register("password")} type="password" name="password" id="" />
+          <input placeholder='password' {...register("password", { required: { value: true, message: "This field is required" }, minLength: { value: 3, message: "Min length is 3" }, maxLength: { value: 16, message: "Max length is 16" } })} type="password" />
+          {errors.password && <div className='text'>{errors.password.message}</div>}
           <br />
-          <input type="submit" value="Submit" />
+          <input disabled={isSubmitting} type="submit" value="Submit" />
         </form>
       </div>
     </>
