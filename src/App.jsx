150dca60e1c53f42b1d03b35f346041678aea38c
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-06-01 15:31:09 +0530

    form submited using react hook form

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,27 @@ function App() {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm()
+  } = useForm();
 
-  const onSubmit = (data) => console.log(data)
+  const delay = (d) => {
+    return new Promise((resolve, reject) => {
+      setTimeout((d) => {
+        resolve()
+      }, d * 1000);
+    })
+  }
+
+  const onSubmit = async (data) => {
+    await delay(4)
+    console.log(data)
+  }
+
 
   return (
     <>
       <div className="container">
         <form action="" onSubmit={handleSubmit(onSubmit)}>
-          <input placeholder='username' {...register("username", { required: true, minLength: { value: 3, message: "Min length is 3" }, maxLength: { value: 16, message: "Max length is 16" } })} type="text" />
+          <input placeholder='username' {...register("username", { required: { value: true, message: "This field is required" }, minLength: { value: 3, message: "Min length is 3" }, maxLength: { value: 16, message: "Max length is 16" } })} type="text" />
           {errors.username && <div className='text'>{errors.username.message}</div>}
           <br />
           <input placeholder='password' {...register("password")} type="password" name="password" id="" />
